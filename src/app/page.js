13614b83a2a88e319009383a2e51644242b1e9e5
git <itslewbie01@gmail.com>
2024-12-08 05:09:55 +0000

    bo3 zombies map picker , will fix css soon

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,103 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useState } from 'react';
+import RoulettePro from 'react-roulette-pro';
+import 'react-roulette-pro/dist/index.css';
+import gracefulLinesPlugin from 'rrp-graceful-lines-plugin';
+import 'rrp-graceful-lines-plugin/dist/index.css';
 
-export default function Home() {
+const prizes = [
+  { image: '/ShadowsOfEvil.webp',
+    text: "Shadows Of Evil"
+   },
+  { image: '/TheGiant.webp',
+    text: "The Giant"
+   },
+  { image: '/DerEisendrache.webp',
+    text: "Der Eisendrache"
+   },
+  { image: '/ZNN.webp',
+    text: "Zetsubou No Shima"
+   },
+  { image: '/GK.webp',
+    text: "Gorod Krovi"
+   },
+  { image: '/Revelations.webp',
+    text: "Revelations"
+   },
+  { image: '/NDU.webp',
+    text: "Nacht Der Untoten"
+   },
+  { image: '/verruckt.webp',
+    text: "Verruckt"
+  },
+  { image: '/SNN.webp',
+    text: "Shi No Numa"
+   },
+  { image: '/Kino.webp',
+    text: "Kino Der Toten"
+   },
+  { image: '/Shang.webp',
+    text: "Shangri La"
+   },
+  { image: '/Ascension.webp',
+    text: "Ascension"
+   },
+  { image: '/Moon.webp',
+    text: "Moon"
+   },
+  { image: '/Origins.webp',
+    text: "Origins"
+   },
+];
+
+const reproductionArray = (array = [], length = 0) => [
+  ...Array(length)
+    .fill('_')
+    .map(() => array[Math.floor(Math.random() * array.length)]),
+];
+
+const reproducedPrizeList = [
+  ...prizes,
+  ...reproductionArray(prizes, prizes.length * 3),
+  ...prizes,
+  ...reproductionArray(prizes, prizes.length),
+];
+
+const generateId = () =>
+  `${Date.now().toString(36)}-${Math.random().toString(36).substring(2)}`;
+
+const prizeList = reproducedPrizeList.map((prize) => ({
+  ...prize,
+  id: typeof crypto.randomUUID === 'function' ? crypto.randomUUID() : generateId(),
+}));
+
+const App = () => {
+  const [start, setStart] = useState(false);
+  const [prizeIndex, setPrizeIndex] = useState(null);
+
+  const handleStart = () => {
+    setPrizeIndex(Math.floor(Math.random() * prizeList.length));
+    setStart((prevState) => !prevState);
+  };
+
+  const handlePrizeDefined = () => {
+    console.log('🥳 Prize defined! 🥳');
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <RoulettePro
+       designPlugin={gracefulLinesPlugin({
+        prizesWithText: true,
+      })}
+        prizes={prizeList}
+        prizeIndex={prizeIndex}
+        start={start}
+        onPrizeDefined={handlePrizeDefined}
+      />
+      <button onClick={handleStart}>Start</button>
+    </>
   );
-}
+};
+
+export default App;
